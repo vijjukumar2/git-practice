acd016a47f4c50272c562fb87a8fd343ea91f310
vijaykumar <vijjukumar2@gmail.com>
2024-08-29 11:47:27 +0530

    nnnn

--- special-variable.sh
+++ special-variable.sh
@@ -2,3 +2,7 @@
 
 echo "All variable passed to the script: $@"
 echo "Number of variables passed: $#"
+echo "script name: $0"
+echo "current working directory: $PWD"
+echo "home directory og current user: $HOME"
+echo "PID of the script: $$"
