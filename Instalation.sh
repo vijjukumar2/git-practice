0f43f6bc008e6b2c84213fb5513f278df1c289ab
vijaykumar <vijjukumar2@gmail.com>
2024-08-30 11:13:31 +0530

    nnnn

--- Instalation.sh
+++ Instalation.sh
@@ -1,4 +1,11 @@
 #!/bin/bash/
 
 USERID=$(id -u)
-echo "USER ID is : $USERID"
+#echo "USER ID is : $USERID"
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script root priveleges"
+fi
+
+dnf install git
