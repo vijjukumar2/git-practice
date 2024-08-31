67ba1891c1a2afceaa3a1d8c481ea6063975ae76
vijaykumar <vijjukumar2@gmail.com>
2024-08-31 15:04:06 +0530

    nnnn

--- loop.sh
+++ loop.sh
@@ -1,8 +1,12 @@
 #!/bib/bash/
 
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
-echo "6"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
+# echo "6"
+for i in 1 2 3 4 5 6 7 8 9 
+do
+   echo $i
+done
