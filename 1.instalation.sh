1c72dc6f74649ba3118a6cb3440743a32ce48fc4
vijaykumar <vijjukumar2@gmail.com>
2024-09-02 13:27:06 +0530

    download mysql

--- 1.instalation.sh
+++ 1.instalation.sh
@@ -13,6 +13,14 @@ fi
  if [ $? -ne 0 ]
  then
     echo "git not installed then install it .."
+     dnf install git -y
+     if [ $? -ne 0 ]
+     then 
+          echo " git instalation not sucess  check it.."
+          exit 1
+     else
+       echo " git instalation is sucess "
+     fi
  else
     echo " git already instaleed nothing to do.."
  fi
